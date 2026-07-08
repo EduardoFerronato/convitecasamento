15739f8dc26622216c0dece81ead7e451e98b7fb
EduardoFerronato <edu.fe.guin@gmail.com>
2026-07-08 15:51:03 -0400

    a

---
+++ src/app/apple-icon.tsx
@@ -0,0 +1,55 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 180, height: 180 };
+export const contentType = "image/png";
+
+export default async function AppleIcon() {
+  const font = await fetch(
+    "https://fonts.gstatic.com/s/cormorantgaramond/v21/co3umX5slCNuHLi8bLeY9MK7whWMhyjypVO7abI26QOD_v86GnM.ttf",
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#050a14",
+        }}
+      >
+        <div
+          style={{
+            width: 136,
+            height: 136,
+            borderRadius: "50%",
+            background:
+              "radial-gradient(circle at 32% 28%, #121c32 0%, #0d1528 55%, #050a14 100%)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            border: "2px solid #121c32",
+          }}
+        >
+          <div
+            style={{
+              fontSize: 42,
+              color: "#e9d4a0",
+              fontFamily: "Cormorant Garamond",
+              letterSpacing: -1,
+              marginTop: 4,
+            }}
+          >
+            A | E
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [{ name: "Cormorant Garamond", data: font, style: "normal", weight: 400 }],
+    },
+  );
+}
